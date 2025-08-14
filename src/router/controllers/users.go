0e182204e2365b0d9f5f521cebf9882a26c284ad
Leandro <leandroj.r.ximenes@gmail.com>
2025-08-14 13:26:54 +0000

    feat(user): implement CreateUser function with request handling and database integration

--- src/router/controllers/users.go
+++ src/router/controllers/users.go
@@ -1,9 +1,41 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"main/src/router/config"
+	"main/src/router/models"
+	"main/src/router/repository"
+	"net/http"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create user"))
+	log.Println("Creating new user")
+	bodyRequest, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+	if err = json.Unmarshal(bodyRequest, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := config.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repository.NewUserRepository(db)
+	id, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("New user created with success")
+	w.Write([]byte(fmt.Sprintf("Id inserted: %d", id)))
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
