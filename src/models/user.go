ba19b799f4e1de03cff7f0947a490e7e4d62fffd
Leandro <leandroj.r.ximenes@gmail.com>
2025-08-21 20:38:16 +0000

    feat(user): add UserDto with validation and formatting methods

--- src/models/user.go
+++ src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	dto "main/src/dtos"
+	"main/src/utils"
+	"time"
+)
 
 type User struct {
 	ID        int       `json:"id,omitempty"`
@@ -9,4 +13,12 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+func (userModel *User) ConvertFromDto(userDto dto.UserDto) {
+	userModel.Name = userDto.Name
+	userModel.Email = userDto.Email
+	userModel.Password = userDto.Password
+	userModel.Slug = utils.Slugify(userDto.Name)
+	userModel.CreatedAt = time.Now()
 }
