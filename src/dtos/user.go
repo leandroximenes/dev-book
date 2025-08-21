50c2a1856369dbd29d4efd92440a33c7bfa5eaa7
Leandro <leandroj.r.ximenes@gmail.com>
2025-08-21 20:36:47 +0000

    feat(user): add UserDto with validation and formatting methods

---
+++ src/dtos/user.go
@@ -0,0 +1,43 @@
+package dto
+
+import (
+	"errors"
+	"strings"
+)
+
+type UserDto struct {
+	Id       uint   `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (user *UserDto) validateRequired() error {
+	if user.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if user.Email == "" {
+		return errors.New("email is required")
+	}
+
+	if user.Password == "" {
+		return errors.New("password is required")
+	}
+
+	return nil
+}
+
+func (user *UserDto) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+}
+
+func (user *UserDto) Prepare() error {
+	if err := user.validateRequired(); err != nil {
+		return err
+	}
+
+	user.format()
+	return nil
+}
