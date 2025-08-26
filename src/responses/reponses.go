6c2f6e8393e078bf9efb8c2efbcecb134ca966ce
Leandro <leandroj.r.ximenes@gmail.com>
2025-08-26 13:17:56 +0000

    fix(responses): ensure Content-Type header is set for JSON responses

--- src/responses/reponses.go
+++ src/responses/reponses.go
@@ -7,6 +7,7 @@ import (
 )
 
 func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
 	if err := json.NewEncoder(w).Encode(dados); err != nil {
